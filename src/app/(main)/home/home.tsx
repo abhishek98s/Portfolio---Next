dea95aaedb4436965b7baac19c3e849353a5b579
abhsihek <abhishek_mi@yonefu.info>
2024-09-20 16:53:57 +0545

    delete the api

--- src/app/(main)/home/home.tsx
+++ src/app/(main)/home/home.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 import Navbar from '@/components/navbar';
 import Introduction from '@/components/introduction';
-import Expertise from '@/components/expertise';
+import Expertise from '@/components/Expertise';
 import Projects from '@/components/projects';
 import Footer from '@/components/footer';
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Metadata } from "next";
 
-import Navbar from '@/components/navbar';
-import Introduction from '@/components/introduction';
-import Expertise from '@/components/expertise';
-import Projects from '@/components/projects';
-import Footer from '@/components/footer';
+import IndexPage from "./(main)/home/home";
 
 export const metadata: Metadata = {
   title: 'Abhishek Shakya',
@@ -15,11 +11,7 @@ export default function Home() {
 
   return (
     <>
-      <Navbar />
-      <Introduction />
-      <Expertise />
-      <Projects />
-      <Footer />
+      <IndexPage />
     </>
   );
 }
