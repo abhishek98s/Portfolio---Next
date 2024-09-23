f7af17b8034dfdb336be04eb3b8c3a4708104479
abhsihek <abhishek_mi@yonefu.info>
2024-09-23 11:02:23 +0545

    added all

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Expertise from "@/components/expertise";
+import Footer from "@/components/footer";
+import Introduction from "@/components/introduction";
+import Navbar from "@/components/navbar";
+import Projects from "@/components/projects";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Introduction />
+      <Expertise />
+      <Projects />
+      <Footer />
+    </>
+  );
+}
