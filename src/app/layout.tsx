a81206f6ee167ac95774cc3fd7dcb2ee82767282
abhishek98s <abhishekshakya98.com@gmail.com>
2025-01-15 14:59:13 +0545

    feat: preload font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.scss";
+/** @format */
+
+import type { Metadata } from 'next';
+import './globals.scss';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
-  title: "Abhishek Shakya",
-  description: "Web developer dedicated to building applications that real world problem.",
+  title: 'Abhishek Shakya',
+  description:
+    'Web developer dedicated to building applications that real world problem.',
 };
 
 export default function RootLayout({
@@ -12,12 +16,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <Head>
+        <link
+          rel='preload'
+          href='src/app/fonts/source-serif-pro/SourceSerifPro-SemiBold.woff'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
+        />
+
+        <link
+          rel='preload'
+          href='src/app/fonts/source-serif-pro/SourceSerifPro-SemiBold.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
+        />
+      </Head>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
